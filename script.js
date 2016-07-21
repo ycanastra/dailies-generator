6121dc9c35a5e0f958f30413a9d5e849137510af
Yushi Canastra <ycanastra@gmail.com>
2016-07-20 21:04:53 -0400

    added error messages

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
-$("#submit").prop("disabled", true);
+$("#submit").prop("disabled", true)
+$('p.errorMessage').hide()
 
-$('form > input').on('input change', buttonEnabler);
-$('form > select').on('change', buttonEnabler);
+$('input').on('input change', buttonEnabler)
+$('select').on('change', buttonEnabler)
+
+$('select').on('change', function() {
+	if ($(this).val() != null) {
+		$(this).parent().find('p').slideUp(150)
+		$(this).css('border', '')	
+	}
+})
+
+$('input').on('input change', function() {
+	if ($(this).val() != '') {
+		$(this).parent().find('p').slideUp(150)
+		$(this).css('border', '')
+	}
+})
+
+$('input').not('#datepicker').blur(function() {
+	if ($(this).val() == '') {
+		$(this).css('border', '1px solid red')
+		$(this).parent().find('p').slideDown(150)
+	}
+})
+
+$('#datepicker').datepicker({
+  onClose: function(dateText) {
+		if (dateText == '') {
+			$(this).css('border', '1px solid red')
+			$(this).parent().find('p').slideDown(150)
+		}
+		else {
+			$(this).parent().find('p').slideUp(150)
+		  $(this).css('border', '')
+		}
+	}
+})
+
+$('select').blur(function() {
+	if ($(this).val() == null) {
+		$(this).css('border', '1px solid red')
+		$(this).parent().find('p').slideDown(150)
+	}
+})
 
 function buttonEnabler() {
-	var inputEmpty = false;
-	var selectEmpty = false;
+	var inputEmpty = false
+	var selectEmpty = false
 
-	$('form > input').each(function() {
+	$('input').each(function() {
 		if ($(this).val() == '') {
-			inputEmpty = true;
+			inputEmpty = true
 		}
-	});
+	})
 
-	$('form > select').each(function() {
+	$('select').each(function() {
 		if ($(this).val() == null) {
-			selectEmpty = true;
+			selectEmpty = true
 		}
-	});
+	})
 
 	if (inputEmpty || selectEmpty) {
 		$('#submit').prop("disabled", true)
