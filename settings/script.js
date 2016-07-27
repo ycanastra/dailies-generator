d450bd686043e59b118f8d77b99d9f0aa342aed6
Yushi Canastra <ycanastra@gmail.com>
2016-07-26 20:00:52 -0400

    shows the shift data now. readyonly though

---
+++ settings/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+	// var next = 1;
+	// $(".add-more").click(function(e){
+	// 		e.preventDefault();
+	// 		var addto = "#field" + next;
+	// 		var addRemove = "#field" + (next);
+	// 		next = next + 1;
+	// 		var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+	// 		var newInput = $(newIn);
+	// 		var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+	// 		var removeButton = $(removeBtn);
+	// 		$(addto).after(newInput);
+	// 		$(addRemove).after(removeButton);
+	// 		$("#field" + next).attr('data-source',$(addto).attr('data-source'));
+	// 		$("#count").val(next);
+	//
+	// 				$('.remove-me').click(function(e){
+	// 						e.preventDefault();
+	// 						var fieldNum = this.id.charAt(this.id.length-1);
+	// 						var fieldID = "#field" + fieldNum;
+	// 						$(this).remove();
+	// 						$(fieldID).remove();
+	// 				});
+	// });
+
+	var url = 'http://159.203.229.225/dailies_new/data/employee_shifts.json'
+
+	$.getJSON(url, function(data) {
+		$.each(data, function(key, val) {
+			var locationId = key.split(' ').join('_');
+			var newTab = $('<li><a data-toggle="tab" href="#' + locationId + '">' + key + '</a></li>');
+			newTab.appendTo('#location-tab');
+
+			var newDiv = $('<div id="' + locationId + '" class="tab-pane fade"></div>');
+			var newHeader = $('<h3>' + key + '</h3>');
+
+			newDiv.append(newHeader);
+
+			newDiv.appendTo('#location-tab-content');
+
+			$.each(val, function(key, val) {
+				var day = key;
+				var dayElem = $('<br><label class="day">' + day + '</label><br>');
+				dayElem.appendTo('#' + locationId);
+
+				$.each(val, function(key, val) {
+					var hourId = 'h' + day + key;
+					var nameId = 'n' + day + key;
+					var buttonId = 'b' + day + key;
+
+					var hourElem = $('<input type="number" min="0" max="23" id="' + hourId + '" class="hour">');
+					var nameElem = $('<input type="text" id="' + nameId + '" class="name">');
+					var addMoreElem = $('<button id="' + buttonId + '"class="btn add-more" type="button">+</button><br>')
+
+					hourElem.val(key);
+					nameElem.val(val);
+
+					hourElem.appendTo('#' + locationId);
+					nameElem.appendTo('#' + locationId);
+					addMoreElem.appendTo('#' + locationId);
+				});
+			});
+		});
+	});
+});
